f6caa3a06dbf890421ee2f5750f3cc51f1ccb0c0
r16kumar <161674577+r16kumar@users.noreply.github.com>
2025-07-14 19:07:56 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://r16kumar2003:pjn2mkW9pkhusR4k@cluster0.bld0ah9.mongodb.net/");
+        await mongoose.connect(Process.env.MONGO_URL);
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
